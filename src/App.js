bf7dca2770467229e6509a36ef20b96c028e2c42
PHMS13 <phms.13@hotmail.com>
2022-10-05 11:17:28 -0300

    comentários

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { Routes, Route } from "react-router-dom";
-import  Home  from "./pages/Home";
+import Home from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { AuthContextComponent } from "./contexts/authContext";
 import { Profile } from "./pages/Profile";
 import { ProtectedRoute } from "./components/ProtectedRoute";
 import AboutUs from "./pages/AboutUs";
-import  ErrorPage  from "./pages/ErrorPage";
+import ErrorPage from "./pages/ErrorPage";
 import NavBar from "./components/NavBar";
 
 import "./App.css";
@@ -17,7 +17,7 @@ import AllPlants from "./pages/Allplants";
 function App() {
   return (
     <div className="App">
-      <NavBar/>
+      <NavBar />
       <AuthContextComponent>
         <Routes>
           <Route path="/" element={<Home />} />
@@ -27,12 +27,16 @@ function App() {
             path="/profile"
             element={<ProtectedRoute component={Profile} />}
           />
-          <Route path="/mygarden/:idGarden" element={<MyGarden />} /> // pagina de detalhe do garden
-          <Route path="/gardener" element={<Gardener />} /> // all users + searchbar
-          <Route path="/gardener/:idUser" element={<UserProfilec />} /> // pagina de um user
+          <Route path="/mygarden/:idGarden" element={<MyGarden />} />{" "}
+          {/*  pagina de detalhe do garden */}
+          <Route path="/gardener" element={<Gardener />} />{" "}
+          {/* all users + searchbar */}
+          <Route path="/gardener/:idUser" element={<UserProfilec />} />{" "}
+          {/* pagina de um user */}
           <Route path="/allplants" element={<AllPlants />} />
           <Route path="/about" element={<AboutUs />} />
-          <Route path="*" element={<ErrorPage />} />git
+          <Route path="*" element={<ErrorPage />} />
+          git
         </Routes>
       </AuthContextComponent>
     </div>
