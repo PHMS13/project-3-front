66534eac917b752316221eaad06e7a6eb4e2ecf4
Leonardo Nunes <itsmeleo_n@outlook.com>
2022-10-05 11:08:35 -0300

    tentativa 2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import  {Home}  from "./pages/Home";
+import { Home }  from "./pages/Home";
 import { Login } from "./pages/Login";
 import { Signup } from "./pages/Signup";
 import { AuthContextComponent } from "./contexts/authContext";
