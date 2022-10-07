d522a5c640256959b7082de5c9602869d7e5bd23
PHMS13 <phms.13@hotmail.com>
2022-10-07 11:08:31 -0300

    ok

--- src/pages/UserProfile/index.js
+++ src/pages/UserProfile/index.js
@@ -1,46 +1,241 @@
 import { useEffect, useState } from "react";
-import { api } from "../../api/api";
-import { Link, useNavigate, useParams } from "react-router-dom";
 
-function UserProfile() {
-  const [user, setUser] = useState({});
+import { api } from "../../api/api";
+import { Link, useNavigate } from "react-router-dom";
+import EditUser from "../../components/EditUser";
+import Card from "react-bootstrap/Card";
+
+import profileImage from "../../assets/05 - Imagem.png";
+import Quiz from "../Quiz";
+
+import { Button, Accordion } from "react-bootstrap";
+
+function Profile() {
+  //const decoratedOnClick = useAccordionButton(eventKey, onClick);
+
+  const [user, setUser] = useState({ username: "", email: "" });
   const [isLoading, setIsLoading] = useState(true);
-  const { idUser } = useParams();
+
+  const [img, setImg] = useState("");
+  const [reload, setReload] = useState(true);
+
+  const [showForm, setShowForm] = useState(false);
+
+  const navigate = useNavigate();
+
+  const [formGarden, setFormGarden] = useState({
+    name: "",
+    local: "",
+  });
+
+  function handleChange(e) {
+    setFormGarden({ ...formGarden, [e.target.name]: e.target.value });
+  }
+
+  console.log(formGarden);
+
+  //states das perguntas
+  const [luminosidade, setLuminosidade] = useState(0);
+  const [cuidado, setCuidado] = useState(0);
+
+  const [form, setForm] = useState({
+    profileImage: "",
+    username: "",
+    age: "",
+    country: "",
+    city: "",
+    residence: "",
+    garden: [],
+  });
 
   useEffect(() => {
     async function fetchUser() {
       setIsLoading(true);
       try {
-        const response = await api.get(`/users/user/${idUser}`);
+        const response = await api.get("/users/profile");
         setUser(response.data);
+
+        setForm(response.data);
         setIsLoading(false);
       } catch (error) {
         console.log(error);
       }
     }
     fetchUser();
-  }, []);
+  }, [reload]);
 
+  /*   function handleAccordion() {
+    console.log("dentro da funcao");
+    setReload(!reload);
+  } */
   console.log(user);
 
+  function handleLogOut(e) {
+    e.preventDefault();
+    localStorage.removeItem("loggedInUser");
+    navigate("/");
+  }
+  console.log(user);
+
+
+
+  console.log(user);
+  console.log(isLoading);
+
   return (
-    <>
-      {!isLoading && (
-        <>
-          <h1> user Profile</h1>
-          <h1>jardins</h1>
-          {user.garden.map((garden) => {
+    <div style={{ backgroundColor: "#EDDDD6" }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <h1 className="AllSub">Meu Perfil</h1>
+
+        <img src={profileImage} alt="plantinha" className="profileImg" />
+      </div>
+
+      <div className="barraSup">
+        <span className="username">{user.username}</span>
+        <span>
+          <strong>Ambiente:</strong> {user.residence} |
+        </span>
+        <span style={{ marginRight: "12px" }}>{user.age} anos</span>
+
+        <Button
+          onClick={() => setShowForm(!showForm)}
+          className="btn btn-light btn-outline-dark btn-sm me-2"
+          style={{
+            backgroundColor: "#7C6053",
+            color: "white",
+            borderColor: "#7C6053",
+            margin: "10px",
+          }}
+        >
+      
+
+      {showForm === true && (
+        <EditUser
+          form={form}
+          id={user._id}
+          setShowForm={setShowForm}
+          setForm={setForm}
+          reload={reload}
+          setReload={setReload}
+          showForm={showForm}
+        />
+      )}
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "stretch",
+          flexWrap: "nowrap",
+        }}
+      >
+        <div className="barraSupinpa" style={{ display: "flex" }}>
+          <h2 className="AllSub">Crie um Jardim</h2>
+          <form onSubmit={handleSubmitGarden}>
+            <div style={{ display: "flex", margin: " 10px" }}>
+              <label> Nome do jardim </label>
+
+              <input
+                name="name"
+                value={formGarden.name}
+                onChange={handleChange}
+              />
+            </div>
+
+            <label id="label" htmlFor="formResidence">
+              Local:
+            </label>
+            <select
+              required
+              id="formSelect"
+              name="local"
+              onChange={handleChange}
+              defaultValue={formGarden.local}
+            >
+              <option value=""></option>
+              <option value="Quintal">Quintal</option>
+              <option value="Varanda">Varanda</option>
+              <option value="Sala">Sala</option>
+              <option value="Quarto">Quarto</option>
+              <option value="Cozinha">Cozinha</option>
+              <option value="Banheiro">Banheiro</option>
+              <option value="Lavanderia">Lavanderia</option>
+              <option value="Outro">Outro</option>
+            </select>
+            <Button
+              type="submit"
+              className="btn btn-light btn-outline-dark btn-sm me-2"
+              style={{
+                backgroundColor: "#7C6053",
+                color: "white",
+                borderColor: "#7C6053",
+              }}
+            >
+              Salvar Jardim
+            </Button>
+          </form>
+        </div>
+      </div>
+      <div>
+        <h1 className="AllSub">Meus Jardins</h1>
+        {!isLoading &&
+          user.garden.map((garden) => {
+            const date = new Date(garden.createdAt);
+
+            const dd = date.getDate();
+            const mm = date.getMonth() + 1; //janeiro = 0, então precisamos adicionar +1. Isso é só com o mês mesmo.
+            const aa = date.getFullYear();
+
+            const hh = date.getHours();
+            const min = date.getMinutes();
+            console.log(garden);
+
             return (
               <div>
-                <p>local: {garden.local}</p>
-                <Link to={`/mygarden/${garden._id}`}>Va para o jardim</Link>
+                <Card style={{ width: "18rem", marginTop: "16px" }}>
+                  <Card.Body>
+                    <Card.Title>
+                      <p className="profileCardTitle">
+                        <strong>{garden.name}</strong>
+                      </p>
+                    </Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">
+                      <p className="profileCardSub">
+                        <strong>local:</strong> {garden.local}
+                      </p>
+                    </Card.Subtitle>
+                    <Card.Text>
+                      <p className="profileCardText">
+                        <strong>postado em:</strong> {dd}/{mm}/{aa} - {hh}:{min}{" "}
+                      </p>
+                    </Card.Text>
+                    <Card.Link>
+                      <Link
+                        to={`/mygarden/${garden._id}`}
+                        className="profileCardLink"
+                      >
+                        Explore seu Jardim
+                      </Link>
+                    </Card.Link>
+                    {garden.comments.length > 0 && <h2>Comentários:</h2>}
+                    {garden.comments.map((comments) => {
+                      return <p>{comments.content}</p>;
+                    })}
+                  </Card.Body>
+                </Card>
               </div>
             );
           })}
-        </>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
 
-export default UserProfile;
+export default Profile;
